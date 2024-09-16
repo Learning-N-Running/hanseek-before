e7ef369503e63530e9a5874aef6a0e8237563a15
Tina Lee <92625071+Learning-N-Running@users.noreply.github.com>
2024-09-16 18:27:18 +0900

    Add: tailwind config

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -19,41 +19,9 @@ export default function Home() {
         <Heading1 style={{ margin: "6px 24px 0 24px", fontWeight: "400" }}>
           Ready to make safe journey?
         </Heading1>
-        {isClicked ? (
-          <Image
-            src="/images/vb_home_after_add.png"
-            alt={"vehicle"}
-            width={768}
-            height={502}
-            style={{ cursor: "pointer" }}
-            onClick={() => setIsClicked(false)}
-          />
-        ) : (
-          <Image
-            src="/images/vb_home_before_add.png"
-            alt={"vehicle"}
-            width={768}
-            height={502}
-            style={{ cursor: "pointer" }}
-            onClick={() => setIsClicked(true)}
-          />
-        )}
-
-        <Image
-          src="/images/vb_home_buttons.png"
-          alt={"buttons"}
-          width={768}
-          height={282}
-          style={{ marginTop: "-48px", cursor: "pointer" }}
-          onClick={() => router.push("/conversation")}
-        />
-        <Image
-          src="/images/vb_home_emergency.png"
-          alt={"emergency"}
-          width={768}
-          height={137}
-          style={{ marginTop: "-40px", cursor: "pointer" }}
-        />
+        <div className="flex items-center justify-center mx-auto my-0 mt-[50px] w-4/5 bg-green-700">
+          안녕하세요~
+        </div>
       </Container>
     </>
   );
