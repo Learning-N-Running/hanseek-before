3e6e7a6abdeeff425d15bedead60c88f3c7525c8
Max <ahdosj@gmail.com>
2024-09-20 22:28:12 +0800

    Feat:Mypage done

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,24 +1,20 @@
 "use client";
 
 import styled, { keyframes } from "styled-components";
-import { useState } from "react";
-import SlideUpModal from "@/components/base/SlideUpModal";
 import Footer from "@/layout/Footer";
 import Image from "next/image";
 import MyPageTab from "@/components/common/MypageTab";
+import { useState } from "react";
 
 interface MyProfile {
   title: string;
 }
 
 export default function Home() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const handleOpenModal = () => setIsModalOpen(true);
-  const handleCloseModal = () => setIsModalOpen(false);
+  const [activeTab, setActiveTab] = useState("Videos");
 
   return (
-    <>
+    <div className="bg-white">
       <nav className="flex items-center p-6 space-x-4 justify-end">
         <Image
           className=""
@@ -36,20 +32,10 @@ export default function Home() {
         />
       </nav>
       <Profile title="Stella.leeee" />
-      <MyPageTab />
-      <Container>
-        <button onClick={handleOpenModal} className="display bg-slate-400">
-          Open Modal
-        </button>
-      </Container>
-
-      <SlideUpModal isOpen={isModalOpen} onClose={handleCloseModal}>
-        <p>Choose Your Date</p>
-        <p>add location</p>
-      </SlideUpModal>
-
+      <MyPageTab activeTab={activeTab} setActiveTab={setActiveTab} />
+      <WithdrawBar />
       <Footer />
-    </>
+    </div>
   );
 }
 
@@ -72,7 +58,7 @@ const Profile = ({ title }: MyProfile) => {
         width={100}
         height={100}
       />
-      <h2 className="text-xl font-bold">{title}</h2>
+      <h2 className="mt-4 text-xl font-bold">{title}</h2>
       <div className="flex mt-2">
         <Image
           className="mr-1"
@@ -94,6 +80,29 @@ const Profile = ({ title }: MyProfile) => {
         <p>
           Follow <strong>456K</strong>
         </p>
+      </div>
+    </div>
+  );
+};
+
+const WithdrawBar = () => {
+  return (
+    <div className="w-[688px] h-[68px] fixed bottom-32 left-11 z-50 flex justify-center">
+      <div className="flex flex-row items-center bg-[#FAFAFB] p-6 justify-between w-full max-w-4xl">
+        <p className="flex flex-row font-medium ml-4 text-xl">
+          Total coins accumulated:{" "}
+          <Image
+            className="ml-2"
+            src="images\usdc-icon.svg"
+            alt="usdc"
+            width={20}
+            height={20}
+          />{" "}
+          <b>120.00</b>
+        </p>
+        <button className="text-xl justify-end bg-[#FF5924] px-6 py-3 text-white rounded-3xl mr-4 hover:bg-orange-500">
+          Withdraw
+        </button>
       </div>
     </div>
   );
